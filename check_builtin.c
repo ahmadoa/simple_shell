cd90a62a68b51b407f40e2e3d22118f9cde4b166
azeadrr <naceur.azzeddine@ofppt-edu.ma>
2023-04-25 12:18:06 -0700

    0x16. C - Simple Shell

---
+++ check_builtin.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * check_builtin - checks if a command is built in
+ * @argv: arguments of the command
+ * @line: read input line
+ * Return: 1 if it succedds, 0 if it doesn't
+ */
+int check_builtin(char **argv, char *line)
+{
+	if (handle_builtin(argv, line))
+		return (1);
+	else if (**argv == '/')
+	{
+		execmd(argv[0], argv);
+		return (1);
+	}
+	return (0);
+}
