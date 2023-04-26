be9853de4757f7f4a32b2d3970c6f2de22c89b66
ahmadoa <ouladaouida@gmail.com>
2023-04-25 20:11:29 -0700

    updated interactivity mode in main.c

--- main.c
+++ main.c
@@ -57,8 +57,7 @@ char **tokenize(char *line)
 void intHandler(int sigint)
 {
 	(void)sigint;
-	write(STDERR_FILENO, "\n", 1);
-	exit(EXIT_SUCCESS);
+	write(STDERR_FILENO, "\n$ ", 3);
 }
 
 /**
@@ -76,41 +75,52 @@ int main(int argc, char *av[], char *envp[])
 	char *lineptr;
 	size_t n = 0;
 	ssize_t nchars_read;
-	char *path = NULL, **argv, *pathargv = NULL, **paths = NULL;
+	char *path = NULL, **c_argv, *pathargv = NULL, **paths = NULL;
 
 	(void)envp;
-	(void)av;
-	if (argc < 1)
-		return (-1);
 
+	if (argc > 1)
+	{
+		c_argv = &av[1];
+		path = get_path();
+		paths = tokenize(path);
+		pathargv = check_path(paths, c_argv[0]);
+
+		if (!pathargv)
+		{
+			perror(av[0]);
+			return (EXIT_FAILURE);
+		}
+		else
+		{
+			execmd(pathargv, c_argv);
+			return (EXIT_SUCCESS);
+		}
+	}
 	if (signal(SIGINT, intHandler) == SIG_ERR)
 	{
 		perror("signal");
-		exit(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
-
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			write(STDERR_FILENO, "$ ", 2);
-		else
-			write(STDERR_FILENO, "", 0);
 		nchars_read = _getline(&lineptr, &n, stdin);
 		if (nchars_read == -1)
-			return (-1);
-		argv = tokenize(lineptr);
-		if (argv == NULL || *argv == NULL || **argv == '\0')
+			return (EXIT_FAILURE);
+		c_argv = tokenize(lineptr);
+		if (c_argv == NULL || *c_argv == NULL || **c_argv == '\0')
 			continue;
-		if (check_builtin(argv, lineptr))
+		if (check_builtin(c_argv, lineptr))
 			continue;
 		path = get_path();
 		paths = tokenize(path);
-		pathargv = check_path(paths, argv[0]);
+		pathargv = check_path(paths, c_argv[0]);
 		if (!pathargv)
 			perror(av[0]);
 		else
-			execmd(pathargv, argv);
+			execmd(pathargv, c_argv);
 	}
-	free(lineptr);
-	return (0);
+	return (EXIT_SUCCESS);
 }
